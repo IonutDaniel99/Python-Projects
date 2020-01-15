cf8cbdead04ef4b621879a4ea807a99df4d3b3ab
Ionut Daniel <ionut.daniel644@gmail.com>
2020-01-15 13:01:43 +0200

    commit 2.1

--- Youtube-PlayList-Backup/main.py
+++ Youtube-PlayList-Backup/main.py
@@ -1,27 +1,42 @@
-from bs4 import BeautifulSoup
 import requests
 import os
+import re
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-Link = "https://www.youtube.com/playlist?list=PL6uvCMyxa2PgiL4tXfsfj0Ol-KMD5dy0Z"
-response = requests.get(Link)
+print("\n==================================================================================================================")
+print("This script Saves Your First 100 songs from a playlist into a .txt file with the same name of it(name of playlist)")
+print("==================================================================================================================\n")
+
+print(
+    "PlayList Link: ( Example: https://www.youtube.com/playlist?list=PL6uvCzzzzzzzzzztXfsfj0Ol-zzzzzzzz )")
+response = requests.get(input("Link: "))
+
+SongList = []
+current_time = datetime.now().strftime("%H:%M:%S / %d-%m-%Y")
 
 
-def SongList(name, author, time):
-    with open('backup.txt', 'w', encoding='utf-8') as f:
-        f.write(str("%s / %s / %s" % (name, author, time)))
+def Output(PlayList_Name, SongsNumber):
+    with open(PlayList_Name + ' - PlayList.txt', 'w', encoding='utf-8') as f:
+        f.write("Backup Date:   " + current_time + "\n\n")
+        for line in SongList:
+            f.write(line.replace("dela", " / ")
+                    [:-4] + " / " + line.replace("dela", " / ")[-4:] + "\n")
+        f.write("\nTotal Songs: " + str(SongsNumber - 1))
+    print("\nPlaylist " + PlayList_Name + " has been updated!")
 
 
-soup = BeautifulSoup(response.content, "html.parser")
-Name = soup.find("h1", {"class": "pl-header-title"}).text.strip()
-Songs = soup.select("table", {"id": "pl-video-table"})
+def main():
+    i = 1
+    soup = BeautifulSoup(response.content, "html.parser")
+    Name = soup.find("h1", {"class": "pl-header-title"}).text.strip()
+    for tr in soup.findAll("table", {"id": "pl-video-table"}):
+        for tr in soup.findAll("tr", {"class": "pl-video"}):
+            SongList.append(str(i)+"." + tr.get_text().translate(
+                {ord(c): None for c in ' \n'}))
+            i += 1
+    Output(Name, i)
 
-for tr in soup.findAll("tbody", {"id": "pl-load-more-destination"}):
-    for td in tr.findAll("tr", {"class": "pl-video yt-uix-tile"}):
-        for a in td.findAll("a", {"class": "pl-video-title-link"})[0]:
-            song_name = a
-        for b in td.findAll("div", {"class": "pl-video-owner"})[0]:
-            song_author = td.findAll("a")[2].get_text()
-        for c in td.findAll("td", {"class": "pl-video-time"})[0]:
-            song_time = td.findAll("div", {"class": "timestamp"})[0].get_text()
-    SongList(song_name, song_author, song_time)
-    print(song_name + song_author + song_time)
+
+if __name__ == "__main__":
+    main()
