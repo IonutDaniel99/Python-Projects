a546cf50d3959c276bd95eb81145543c6982e704
Ionut Daniel <ionut.daniel644@gmail.com>
2020-01-18 15:20:18 +0200

    Commit 4.0

---
+++ Bitcoin-Market-Analyser-Bot/main.py
@@ -0,0 +1,63 @@
+import requests
+import sys
+import os
+import time
+from datetime import datetime
+
+Api_url = 'https://api.coinmarketcap.com/v1/ticker/'
+
+if sys.platform.lower() == "win32":
+    os.system('color')
+
+
+class style():
+    @staticmethod
+    def RED(x): return '\033[31m' + str(x)
+    @staticmethod
+    def GREEN(x): return '\033[32m' + str(x)
+    @staticmethod
+    def YELLOW(x): return '\033[33m' + str(x)
+    @staticmethod
+    def RESET(x): return '\033[0m' + str(x)
+
+
+def main():
+    Price_At_start = requests.get(Api_url + 'bitcoin').json()[0]['price_usd']
+    Lowest_Today = Price_At_start
+    Highest_Today = Price_At_start
+    last_price = 0
+    update = input("Update Rate In Seconds: ")
+    update = int(update)
+    while True:
+        try:
+            now = datetime.now()
+            response = requests.get(Api_url + 'bitcoin')
+            response_json = response.json()
+            price = response_json[0]['price_usd']
+            if price != last_price:
+                print(now.strftime('%Y-%m-%d %H:%M:%S / ') +
+                      style.GREEN("Price Changed") + style.RESET(""))
+                if price <= str(last_price):
+                    print(now.strftime('%Y-%m-%d %H:%M:%S') +
+                          ' / Bitcoin price = ' + style.YELLOW(price) + style.RESET("") + ' USD / ' + style.RED(" Decrease") + style.RESET(""))
+                else:
+                    print(now.strftime('%Y-%m-%d %H:%M:%S') +
+                          ' / Bitcoin price = ' + style.YELLOW(price) + style.RESET("") + ' USD / ' + style.GREEN(" Increase") + style.RESET(""))
+                if str(Lowest_Today) > price:
+                    Lowest_Today = price
+                if price > str(Highest_Today):
+                    Highest_Today = price
+                last_price = price
+            time.sleep(update)
+        except KeyboardInterrupt:
+            print("Lowest Today: " + Lowest_Today +
+                  "\n" + "Highest Today: " + Highest_Today)
+            quit()
+        except:
+            print(now.strftime('%Y-%m-%d %H:%M:%S / ') +
+                  "Internet connection not detected.")
+            time.sleep(update)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Processes and Servicess killer is a script where u add Process name in **Process
 
 A script to backup your Youtube Playlist via Link into a txt file(Only text,not download)
 
+##Wireless-Netowrk-List
+
+Shows nearby Wirelesses Name,Mac,Signal,Authentification Protection,Others
+
+##Bitcoin-Market-Analyser-Bot
+
+Every 60 Seconds it prints Bitcon Price in USD
